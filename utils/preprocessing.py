5230bfc2319127b8bdc40bed61ef7904135bb69c
murakamixi <s170073@st.yamagata-u.ac.jp>
2022-06-27 16:20:40 +0900

    delete:不要な呼び出しを削除しました

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,22 +1,9 @@
 # パッケージのimport
-import random
-import math
-import time
-from zlib import Z_DEFAULT_STRATEGY
-import pandas as pd
-import numpy as np
 from PIL import Image
 
-import torch
 import torch.utils.data as data
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 
 from torchvision import transforms
-
-from tqdm import tqdm
-from collections import OrderedDict
 
 class ImageTransform():
     """画像の前処理クラス"""
