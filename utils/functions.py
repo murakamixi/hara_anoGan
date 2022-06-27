24365c4bb2f3eb1851dc0c944790bd190e1c2b3d
murakamixi <s170073@st.yamagata-u.ac.jp>
2022-06-27 16:13:02 +0900

    delete:前処理に関するクラスを削除しました

--- utils/functions.py
+++ utils/functions.py
@@ -39,40 +39,6 @@ def make_datapath_list():
 
     return train_img_list
 
-class ImageTransform():
-    """画像の前処理クラス"""
-
-    def __init__(self, mean, std):
-        self.data_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
-
-    def __call__(self, img):
-        return self.data_transform(img)
-
-
-class GAN_Img_Dataset(data.Dataset):
-    """画像のDatasetクラス。PyTorchのDatasetクラスを継承"""
-
-    def __init__(self, file_list, transform):
-        self.file_list = file_list
-        self.transform = transform
-
-    def __len__(self):
-        '''画像の枚数を返す'''
-        return len(self.file_list)
-
-    def __getitem__(self, index):
-        '''前処理をした画像のTensor形式のデータを取得'''
-
-        img_path = self.file_list[index]
-        img = Image.open(img_path)  # [高さ][幅]白黒
-
-        # 画像の前処理
-        img_transformed = self.transform(img)
-
-        return img_transformed
 
 # ネットワークの初期化
 def weights_init(m):
@@ -235,40 +201,6 @@ def efficient_make_datapath_list():
 
     return train_img_list
 
-class EfficientImageTransform():
-    """画像の前処理クラス"""
-
-    def __init__(self, mean, std):
-        self.data_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
-
-    def __call__(self, img):
-        return self.data_transform(img)
-
-
-class Efficient_GAN_Img_Dataset(data.Dataset):
-    """画像のDatasetクラス。PyTorchのDatasetクラスを継承"""
-
-    def __init__(self, file_list, transform):
-        self.file_list = file_list
-        self.transform = transform
-
-    def __len__(self):
-        '''画像の枚数を返す'''
-        return len(self.file_list)
-
-    def __getitem__(self, index):
-        '''前処理をした画像のTensor形式のデータを取得'''
-
-        img_path = self.file_list[index]
-        img = Image.open(img_path)  # [高さ][幅]白黒
-
-        # 画像の前処理
-        img_transformed = self.transform(img)
-
-        return img_transformed
 
 def efficient_train_model(G, D, E, dataloader, num_epochs):
 
